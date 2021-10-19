7d0ff46d5e46d64a9478b8f3cd42f90dfe7b2e6c
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2021-10-19 13:26:37 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,46 @@
-const medtehnika = [
-    "inhaler",
-    "tonometer",
-    "glucometer",
-];
-console.log(medtehnika);
+const medtehnika = document.querySelector('.medtehnika');
+
+const medtehnika1 = {
+    name: 'inhaler',
+    price: 100,
+    img: src = "https://medmag.ua/image/cache/data/import_yml/178/081/187/681358659_w640_h640_omron_ne_c28p-380x380.jpeg.webp"
+        
+};
+const medtehnika2 = {
+    name: 'tonometer',
+    price: 120,
+    img:src = "https://medmag.ua/image/cache/catalog/image/tonometr/avtomaticheskie/oromed-oro-n2-basic-380x380.jpg.webp"
+    
+};
+const medtehnika3 = {
+    name: 'glucometer',
+    price: 200,
+    img:src ="https://medmag.ua/image/cache/catalog/image/glukometr/infopia-element-380x380.png.webp"
+    
+};
+
+
+function medtehnikaOl(medtehnikaName, medtehnikaObj) {
+    const tehnika = createElement('div', medtehnikaName);
+    const card = createElement('div', 'card');
+    const name = createElement('div', 'name');
+    const price = createElement('div', 'price');
+    const img = createElement('img');
+
+    name.innerText = medtehnikaObj.name;
+    price.innerText = medtehnikaObj.price;
+    img.src = medtehnikaObj.img;
+
+    tehnika.appendChild('card');
+    card.appendChild('card');
+    name.appendChild('name');
+    price.appendChild('price');
+    img.appendChild('img');
+
+    return tehnika;
+    
+
+};
+medtehnika.appendChild('medtehnika1',medtehnika1)
+medtehnika.appendChild(medtehnika2)
+medtehnika.appendChild(medtehnika3)
