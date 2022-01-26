df64c9aabfa7dd53f79411c8a045742c524db948
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2022-01-26 10:50:01 +0200

    50

--- js/main.js
+++ js/main.js
@@ -25,9 +25,12 @@ function medicalEquipmentOl(medicalEquipmentName, medicalEquipmentObj) {
     medicalEquipment.classList.add(medicalEquipmentName);
     var cards = document.createElement('div');
     cards.classList.add('cards');
-    var card = document.createElement('a');
-    card.href = "./partner.html";
-    card.classList.add('card1');
+    var card1 = document.createElement('a');
+    card1.href = "./partner.html";
+    card1.classList.add('card1');
+    var card2 = document.createElement('a');
+    card2.href = "/"
+    card2.classList.add('capd2');
     var name = document.createElement('h2');
     name.classList.add('name');
     var price = document.createElement('p',);
@@ -40,10 +43,15 @@ function medicalEquipmentOl(medicalEquipmentName, medicalEquipmentObj) {
     price.innerText = medicalEquipmentObj.price;
     img.src = medicalEquipmentObj.img;
 
-    medicalEquipment.appendChild(card);
-    card.appendChild(img);
-    card.appendChild(name);
-    card.appendChild(price);
+    medicalEquipment.appendChild(card1);
+    card1.appendChild(img);
+    card1.appendChild(name);
+    card1.appendChild(price);
+
+    medicalEquipment.appendChild(card2);
+    card2.appendChild(img);
+    card2.appendChild(name);
+    card2.appendChild(price);
    
 
 
@@ -51,7 +59,7 @@ function medicalEquipmentOl(medicalEquipmentName, medicalEquipmentObj) {
     pole.appendChild(medicalEquipment)
 
 
-    card.onclick = function transition() {
+    card1.onclick = function transition() {
         document.location.href = "./partner.html";
         
     }
