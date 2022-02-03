8f2b75420589891df903b76aa1ea671a365d301b
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2022-02-03 17:45:27 +0200

    100

--- js/main.js
+++ js/main.js
@@ -23,18 +23,26 @@ var medicalEquipment3 = {
 function medicalEquipmentOl(medicalEquipmentName, medicalEquipmentObj) {
     var medicalEquipment = document.createElement('div');
     medicalEquipment.classList.add(medicalEquipmentName);
+
     var cards = document.createElement('div');
     cards.classList.add('cards');
+
+    var cardName = document.createElement('ul');
+    cardName.classList.add('cardName');
+
+    var cardName1 = document.createElement('li');
+    cardName1.classList.add('cardName1');
+
     var card1 = document.createElement('a');
     card1.href = "./partner.html";
     card1.classList.add('card1');
-    var card2 = document.createElement('a');
-    card2.href = "/"
-    card2.classList.add("capd2");
+
     var name = document.createElement('h2');
     name.classList.add('name');
+
     var price = document.createElement('p',);
     price.classList.add('price');
+
     var img = document.createElement('img');
     img.classList.add('img');
 
@@ -43,18 +51,12 @@ function medicalEquipmentOl(medicalEquipmentName, medicalEquipmentObj) {
     price.innerText = medicalEquipmentObj.price;
     img.src = medicalEquipmentObj.img;
 
-    medicalEquipment.appendChild(card1);
-    card1.appendChild(img);
-    card1.appendChild(name);
-    card1.appendChild(price);
+    medicalEquipment.appendChild(cardName);
+    cardName.appendChild(img);
+    cardName.appendChild(name);
+    cardName.appendChild(price);
 
-    var getElement = function () {
-        medicalEquipment2.appendChild(card2);
-        card2.appendChild(img);
-        card2.appendChild(name);
-        card2.appendChild(price);
-    }
-    getElement()
+   
 
     var pole = document.querySelector('#pole');
     pole.appendChild(medicalEquipment)
